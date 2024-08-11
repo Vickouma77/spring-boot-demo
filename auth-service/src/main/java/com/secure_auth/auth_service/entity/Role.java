97180171459295d68a2965e9d49c706c39f21600
Vickouma77 <Vickprestone20@gmail.com>
2024-08-11 16:17:04 +0300

    perf(entity): creating model jpa entities for user and role

---
+++ auth-service/src/main/java/com/secure_auth/auth_service/entity/Role.java
@@ -0,0 +1,21 @@
+package com.secure_auth.auth_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    private String name;
+}
