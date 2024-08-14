e123dc48c62d6c887f7383c2a0555b503721fc13
Vickouma77 <Vickprestone20@gmail.com>
2024-08-14 15:03:39 +0300

    perf(dto): dto layer

---
+++ auth-service/src/main/java/com/secure_auth/auth_service/dto/JwtAuthResponse.java
@@ -0,0 +1,16 @@
+package com.secure_auth.auth_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthResponse {
+
+    private String accessToken;
+    private String tokenType = "Bearer";
+}
