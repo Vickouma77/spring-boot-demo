0e829e0b69f1098234b8d902c1d6871f750109f0
Vickouma77 <Vickprestone20@gmail.com>
2024-08-14 15:58:22 +0300

    fix(CustomUserDetailsService): adding missing UserDetailService bean

--- auth-service/src/main/java/com/secure_auth/auth_service/services/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/secure_auth/auth_service/services/CustomUserDetailsService.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private UserRepository userRepository;
 
