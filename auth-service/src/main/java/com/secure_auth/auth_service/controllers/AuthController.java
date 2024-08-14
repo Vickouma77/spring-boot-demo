28f3578580b11b4e35b38c372bf98fc2799edaef
Vickouma77 <Vickprestone20@gmail.com>
2024-08-14 15:45:32 +0300

    perf(controllers): controller layer

---
+++ auth-service/src/main/java/com/secure_auth/auth_service/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package com.secure_auth.auth_service.controllers;
+
+
+import com.secure_auth.auth_service.dto.JwtAuthResponse;
+import com.secure_auth.auth_service.dto.LoginDto;
+import com.secure_auth.auth_service.services.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtAuthResponse> authenticate(@RequestBody LoginDto loginDto) {
+        String token = authService.login(loginDto);
+
+        JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
+        jwtAuthResponse.setAccessToken(token);
+
+        return ResponseEntity.ok(jwtAuthResponse);
+    }
+}
