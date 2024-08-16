a63e842906a0b395aedbd93e714cb09baf4f7e64
Vickouma77 <Vickprestone20@gmail.com>
2024-08-16 17:42:14 +0300

    refactor(pact-cdc-demo): demo for consumer-driver-contract tests with pacts

---
+++ pact-cdc-demo/src/test/kotlin/com/secure_auth/pact_cdc_demo/PactCdcDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.secure_auth.pact_cdc_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PactCdcDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
