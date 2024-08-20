cf89a6bb04eb103753060c8439cd8a67df9c4ee8
Vickouma77 <Vickprestone20@gmail.com>
2024-08-20 14:05:08 +0300

    security auth initial commit

---
+++ security-auth/src/main/kotlin/com/secure_auth/security_auth/SecurityAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.secure_auth.security_auth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecurityAuthApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecurityAuthApplication>(*args)
+}
