761f65633cd254d3c5bce6f54c8f009c2d3c7bd7
Vickouma77 <Vickprestone20@gmail.com>
2024-08-27 18:27:42 +0300

    fix(BankAccountRepository): set up repository

--- axon-starter/src/main/kotlin/com/secure_auth/axon_starter/query/repository/BankAccountRepository.kt
+++ axon-starter/src/main/kotlin/com/secure_auth/axon_starter/query/repository/BankAccountRepository.kt
@@ -1,8 +1,8 @@
 package com.secure_auth.axon_starter.query.repository
 
 import com.secure_auth.axon_starter.query.model.BankAccountProjection
+import org.springframework.data.jpa.repository.JpaRepository
 
 interface BankAccountRepository {
-    fun findById(accountId: String): BankAccountProjection
-    //TODO
+    fun findById(accountId: String): JpaRepository<BankAccountProjection, String>
 }
