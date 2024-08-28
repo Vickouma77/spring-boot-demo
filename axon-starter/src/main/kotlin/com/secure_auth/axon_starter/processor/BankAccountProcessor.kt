1bb8ccb0c01e469369e3b3eb95df00cccb122d31
Vickouma77 <Vickprestone20@gmail.com>
2024-08-28 12:38:19 +0300

    docs(processor): class and method level documentation

--- axon-starter/src/main/kotlin/com/secure_auth/axon_starter/processor/BankAccountProcessor.kt
+++ axon-starter/src/main/kotlin/com/secure_auth/axon_starter/processor/BankAccountProcessor.kt
@@ -6,15 +6,50 @@ import com.secure_auth.axon_starter.query.model.BankAccountProjection
 import com.secure_auth.axon_starter.query.repository.BankAccountRepository
 import org.axonframework.eventhandling.EventHandler
 import org.springframework.stereotype.Component
-
+/**
+ * Processor responsible for handling events related to bank accounts and updating the query model.
+ *
+ * The [BankAccountProcessor] listens to events such as [AccountCreatedEvent] and
+ * [MoneyDepositedEvent] and updates the corresponding projections in the database.
+ *
+ * This processor uses the [BankAccountRepository] to save and update [BankAccountProjection]
+ * objects, which represent the state of bank accounts in a read-optimized format.
+ *
+ * Each event handler method in this class is annotated with {EventHandler}, indicating that it will
+ * be triggered when the respective event is published by the event bus.
+ *
+ * @see AccountCreatedEvent
+ * @see MoneyDepositedEvent
+ * @see BankAccountProjection
+ * @see BankAccountRepository
+ * @since 1.0
+ */
 @Component
 class BankAccountProcessor(private val repository: BankAccountRepository) {
 
+    /**
+     * Event handler for the {AccountCreatedEvent}.
+     *
+     * When a new bank account is created, this handler saves a {BankAccountProjection}
+     * with the account ID and initial balance to the repository.
+     *
+     *
+     * @param event The event containing the account creation details.
+     */
     @EventHandler
     fun on(event: AccountCreatedEvent) {
         repository.save(BankAccountProjection(event.accountId, event.initialBalance))
     }
 
+    /**
+     * Event handler for the {MoneyDepositedEvent}.
+     *
+     * When money is deposited into a bank account, this handler retrieves the corresponding
+     * {BankAccountProjection} from the repository, updates its balance, and saves the updated projection.
+     *
+     * @param event The event containing the deposit details.
+     * @throws NoSuchElementException if the account cannot be found in the repository.
+     */
     @EventHandler
     fun on(event: MoneyDepositedEvent) {
         val account = repository.findById(event.accountId).orElseThrow()
