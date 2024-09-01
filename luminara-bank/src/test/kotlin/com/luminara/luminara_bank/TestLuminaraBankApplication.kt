46ea77cc709bdee9adb7145922168174634a43b7
Vickouma77 <Vickprestone20@gmail.com>
2024-09-01 10:14:41 +0300

    delete

--- luminara-bank/src/test/kotlin/com/luminara/luminara_bank/TestLuminaraBankApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.luminara.luminara_bank
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-
-fun main(args: Array<String>) {
-	fromApplication<LuminaraBankApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
