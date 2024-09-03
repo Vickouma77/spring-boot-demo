109bd27b37134196ef1c9f30f6a33897ecfe4563
Vickouma77 <Vickprestone20@gmail.com>
2024-09-03 13:27:40 +0300

    refactor(commands): moving all commands to BankingCommand

--- axon-starter/src/main/kotlin/com/secure_auth/axon_starter/commands/WithdrawMoneyCommand.kt
+++
@@ -1,8 +0,0 @@
-package com.secure_auth.axon_starter.commands
-
-import org.axonframework.modelling.command.TargetAggregateIdentifier
-
-data class WithdrawMoneyCommand(
-    @TargetAggregateIdentifier val accountId: String,
-    val amountToWithdraw: Double
-)
