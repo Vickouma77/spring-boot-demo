e20017f3c0e9b6bf06ace0bba4cf12de72d8c1a2
Vick <Vickprestone20@gmail.com>
2024-10-07 19:49:15 +0300

    docs(IntegrationTest): annotation class documentation
    Signed-off-by: Vick <Vickprestone20@gmail.com>
    
    #

--- file-upload/src/test/kotlin/com/upload/file_upload/integration/annotation/IntegrationTest.kt
+++ file-upload/src/test/kotlin/com/upload/file_upload/integration/annotation/IntegrationTest.kt
@@ -5,6 +5,14 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 
+/**
+ * Annotation for integration tests.
+ *
+ * This annotation combines several other annotations commonly used in integration tests:
+ * - \@SpringBootTest: Indicates that the test should start a Spring Boot application context.
+ * - \@ContextConfiguration: Specifies the initializers to use for the application context.
+ * - \@ActiveProfiles: Activates the "test" profile.
+ */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @SpringBootTest
