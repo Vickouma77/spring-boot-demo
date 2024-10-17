311737976808f079d998d64982e38fe8215ab660
Vick <Vickprestone20@gmail.com>
2024-10-17 09:53:46 +0300

    build(build.gradle.kts): aws S3 integration
    Signed-off-by: Vick <Vickprestone20@gmail.com>

---
+++ graphql-upload/src/main/kotlin/com/prestone/graphql_upload/config/GraphqlConfig.kt
@@ -0,0 +1,9 @@
+package com.prestone.graphql_upload.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class GraphqlConfig {
+}
